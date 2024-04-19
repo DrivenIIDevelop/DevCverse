e0e8b401d787a8e381ac38379d43164b17ccbc0e
Leslie Chou <chouyuchien@gmail.com>
2024-04-19 11:43:29 -0700

    created Home and NavBar components and implemented tailwind CSS

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NavBar() {
+  return (
+    <div className="bg-slate-300">
+      NavBar
+    </div>
+  )
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-slate-400">
+      Home
+    </div>
+  )
+}
