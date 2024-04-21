7ab925af373a20103432985b964e6ff1b1053c75
Leslie Chou <chouyuchien@gmail.com>
2024-04-21 10:13:57 -0700

    completed superior navigation section in the NavBar

--- frontend/src/components/PromoBanner.jsx
+++ frontend/src/components/PromoBanner.jsx
@@ -2,8 +2,8 @@
 
 export default function PromoBanner() {
   return (
-    <p className="bg-[#F2DBEB] text-center text-[#732E5C] text-sm font-light pt-1.5 pb-1.5 max-w-[1440px] mx-auto">
-      FREE SHIPPING ON ALL ORDERS WITH PROMO CODE: SHIPFORALL
+    <p className="bg-[#F2DBEB] text-center text-[#732E5C] text-sm font-light font-epilogue pt-1.5 pb-1.5 max-w-[1440px] mx-auto">
+      FREE SHIPPING ON ALL ORDERS WITH PROMO CODE: <span className="font-epilogue font-normal text-[14px] leading-4">SHIPFORALL</span>
     </p>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,11 @@ export default {
   ],
   
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Epilogue', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
