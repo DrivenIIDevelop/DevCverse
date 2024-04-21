33fe93b2ee36e605cbefb8658304a0ad591a60ef
Leslie Chou <chouyuchien@gmail.com>
2024-04-20 17:48:29 -0700

    added PromoBanner component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import Home from './pages/Home';
 import NavBar from './components/NavBar';
 
 import './App.css';
+import PromoBanner from './components/PromoBanner';
 
 function App() {
 
   return (
     <>
+      <PromoBanner />
       <NavBar />
       <Routes>
         <Route exact={true} path='/' element={<Home />} />
---
+++ frontend/src/components/PromoBanner.jsx
@@ -0,0 +1,9 @@
+
+
+export default function PromoBanner() {
+  return (
+    <p className="bg-[#F2DBEB] text-center text-[#732E5C] text-sm font-light pt-1.5 pb-1.5 max-w-[1440px] mx-auto">
+      FREE SHIPPING ON ALL ORDERS WITH PROMO CODE: SHIPFORALL
+    </p>
+  )
+}
