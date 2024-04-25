f108693e6796d95dcf4e83b4cd8576907cf38ae2
Leslie Chou <chouyuchien@gmail.com>
2024-04-24 21:11:58 -0700

    completed SingleProduct page and styling

--- frontend/src/components/SingleProduct/SingleProductCard.jsx
+++ frontend/src/components/SingleProduct/SingleProductCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { img1, img2, img3, img4, img5, img6, img7 } from "./singleProductImage";
 import { FaStar } from "react-icons/fa";
 import badges from "../../assets/Badges.png";
@@ -7,8 +7,10 @@ import cartIcon from "../../assets/cart.svg";
 import like from "../../assets/like.svg";
 import { FaMinus } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa6";
-import checked from '../../assets/Radio_checked.png';
-import notChecked from '../../assets/Radio_checked.png';
+import checked from "../../assets/RadioCheck.svg";
+import notChecked from "../../assets/RadioNotChecked.svg";
+import rectangle from "../../assets/Rectangle.svg";
+import horizontal_badge from "../../assets/Horizontal Badge.svg";
 
 const product = {
   name: "Glass Skin Water-Gel Moisturizer",
@@ -19,12 +21,15 @@ const product = {
   imageUrl: [img1, img2, img3, img4, img5, img6, img7],
 };
 
+
+const productDescription = `Instantly hydrates, balances and strengthens your skin with the power of vegan pre- and probiotics and hyaluronic acid. The perfect end to your skincare routine, our Glass Skin Water-Gel Moisturizer provides instant and long-lasting hydration for a Glass Skin glow. This breakthrough formula delivers a proprietary vegan prebiotic and probiotic complex that strengthens your skin’s unique microbiome – the environment in which your skin functions – to strengthen skin and curb breakouts. The moisturizer is also expertly blended with natural antioxidants and vitamins found in peach and kiwi extracts, combined with calming chamomile, and licorice that helps skin appear more even-toned and brighter. Meanwhile, 3 different weights of hyaluronic acid hydrates deeply while beta glucans help bind moisture to skin for long-lasting hydration. The finishing touch is powered by our signature VoluSmooth™ that leaves skin looking and feeling smooth.`;
+
 export default function SingleProductCard() {
   const [productQuantity, setProductQuantity] = useState(1);
   const [category, setCategory] = useState("Details");
   const [productPrice, setProductPrice] = useState(product.price.toFixed(2));
 
-  console.log("productPrice", productPrice);
+  // console.log("productPrice", productPrice);
 
   function increaseQuantity() {
     setProductQuantity(productQuantity + 1);
@@ -44,6 +49,21 @@ export default function SingleProductCard() {
     setProductPrice(price.toFixed(2));
   }
 
+  function autoParagraphs( text ) {
+    const paragraphs = text.split(/(?<=\.)\s+/); 
+  
+    return (
+      <div className="font-sans text-sm text-[#333]">
+        {paragraphs.map((para, index) => (
+          <>
+          <p key={index}>{para}</p>
+          {/* <br></br> */}
+          </>
+        ))}
+      </div>
+    );
+  }
+  
   return (
     <div className="flex flex-col pr-14">
       <span className="font-sans text-sm text-[#BF4C9A] flex gap-1 mb-2 items-center">
@@ -65,33 +85,50 @@ export default function SingleProductCard() {
       <img src={badges} alt="product badges" className="mb-2" />
       {/* <img src={purchaseOption} alt="" className="mb-2" /> */}
       <div className="mb-2">
-        <div className="flex gap-2 items-center">
-          <input
-            name="price"
-            type="radio"
-            value={product.price}
-            onClick={() => handlePriceChange(product.price)}
-            defaultChecked
-            className="hidden"
-          />
-
-        <img src={productPrice === product.price ? {checked} : {notChecked}} alt="one time purchase" />
-          {/* <p className="text-sm">One-time purchase</p> */}
+        <div
+          className={` flex gap-2 items-center justify-between text-sm mb-2 mt-2 ${
+            productPrice === product.price.toFixed(2)
+              ? "text-[#CC70AE]"
+              : "text-[#333]"
+          }`}
+        >
+          <div className="flex gap-2 items-center">
+            <img
+              src={
+                productPrice === product.price.toFixed(2) ? checked : notChecked
+              }
+              alt="one time purchase"
+              onClick={() => handlePriceChange(product.price)}
+            />
+            <p >One-time purchase</p>
+          </div>
           <p>${product.price.toFixed(2)}</p>
         </div>
-        <div className="flex gap-2 items-center" style={{}}>
-          <input
-            name="price"
-            type="radio"
-            value={product.price * 0.9}
-            onClick={() => handlePriceChange(product.price * 0.9)}
-          />
-          <img src={productPrice === product.price * 0.9 ? {checked} : {notChecked}} alt="Subscribe & save 10%" />
-          <p className="text-sm">Subscribe & Save 10%</p>
+        <div
+          className={` flex gap-2 items-center justify-between text-sm mb-2 ${
+            productPrice === (product.price * 0.9).toFixed(2)
+              ? "text-[#CC70AE]"
+              : "text-[#333]"
+          }`}
+        >
+          <div className="flex items-center gap-2 relative">
+            <img
+              src={
+                productPrice === (product.price * 0.9).toFixed(2)
+                  ? checked
+                  : notChecked
+              }
+              alt="Subscribe & save 10%"
+              onClick={() => handlePriceChange(product.price * 0.9)}
+              className="w-[24px] h-[24px]"
+            />
+            <p className="text-sm">Subscribe & Save 10%</p>
+            <img src={horizontal_badge} alt="" className="relative bottom-[8px]"/>
+          </div>
           <p>${(product.price * 0.9).toFixed(2)}</p>
         </div>
       </div>
-      <div className="flex justify-between gap-3 mb-2">
+      <div className="flex justify-between gap-3 mb-3">
         <span className="flex gap-5 border border-black rounded-lg py-2 justify-center items-center text-center w-[20%]">
           <FaMinus onClick={decreaseQuantity} />{" "}
           <div className="flex justify-end w-[10px]">{productQuantity}</div>
@@ -104,7 +141,7 @@ export default function SingleProductCard() {
           <img src={like} alt="like button" />
         </button>
       </div>
-      <p className="font-sans text-sm text-[#333] mb-3">
+      <p className="font-sans text-sm text-[#333] mb-4">
         Pay with AfterPay Interest-free payments.{" "}
         <span className="underline">Learn more</span>
       </p>
@@ -143,20 +180,7 @@ export default function SingleProductCard() {
         </p>
       </div>
       <p className="font-sans text-base text-[#333]">
-        Instantly hydrates, balances and strengthens your skin with the power of
-        vegan pre- and probiotics and hyaluronic acid. The perfect end to your
-        skincare routine, our Glass Skin Water-Gel Moisturizer provides instant
-        and long-lasting hydration for a Glass Skin glow. This breakthrough
-        formula delivers a proprietary vegan prebiotic and probiotic complex
-        that strengthens your skin’s unique microbiome – the environment in
-        which your skin functions – to strengthen skin and curb breakouts. The
-        moisturizer is also expertly blended with natural antioxidants and
-        vitamins found in peach and kiwi extracts, combined with calming
-        chamomile, and licorice that helps skin appear more even-toned and
-        brighter. Meanwhile, 3 different weights of hyaluronic acid hydrates
-        deeply while beta glucans help bind moisture to skin for long-lasting
-        hydration. The finishing touch is powered by our signature VoluSmooth™
-        that leaves skin looking and feeling smooth.
+        {autoParagraphs(productDescription)}
       </p>
     </div>
   );
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Footer from '../components/Footer'
 
 export default function Cart() {
