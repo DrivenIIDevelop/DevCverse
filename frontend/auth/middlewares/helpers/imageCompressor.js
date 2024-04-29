560678ad2206eddf69aef51fcef707ebe1f930f0
Guilherme <guifreelacing2016@gmail.com>
2024-04-29 19:17:18 -0300

    temp

--- frontend/auth/middlewares/helpers/imageCompressor.js
+++
@@ -1,1 +0,0 @@
-const Jimp=require("jimp"),path=require("path");module.exports=(async(e,r,i,o,t)=>(Jimp.read(i).then(i=>{i.resize(r,Jimp.AUTO).write(path.resolve(o,`${t}`,e))}).catch(e=>{console.log("Error at reducing size / converting picture : "),console.log(e)}),`${t}/${e}`));
