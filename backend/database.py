ad5d137105382c1b8422c45fb0c1b98211853a6d
Winnie <winnieng3210@gmail.com>
2024-04-29 21:02:32 -0400

    seed data

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./renai.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-#  the connect_args={"check_same_thread": False} argument is EXCLUSIVE ONLY for SQLite.
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
